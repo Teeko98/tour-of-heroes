e994e1de058ac5aacc3d8d8df597e810b1fae3c1
Alberto Sosa <aesosa@proton.me>
2024-02-19 17:39:49 -0500

    Implemented HTTP Get to retrieve hero data from mock server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,21 +7,30 @@ import { Observable, of } from 'rxjs';
 
 import { MessageService } from './message.service';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({ providedIn: 'root' })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient, 
+    private messageService: MessageService) { }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private heroesUrl = 'api/heroes';  // URL to web api
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add('HeroService: fetched hero id=' + id);
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
   }
 }
